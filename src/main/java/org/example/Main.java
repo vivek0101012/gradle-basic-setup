9ba8e375ba989bc5e262c43d66e2b74ced5e66eb
Vivek Nishad <vivekmmmut2027@gmail.com>
2026-06-17 00:34:03 +0530

    add:reterofit http config

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,68 @@
 package org.example;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 import okhttp3.OkHttp;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.Response;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.io.IOException;
+//
+//interface  TodoAPI{
+//    @GET("todos/{id}")
+//    Call<Todo> getTodo (
+//            @Path("id") int id
+//    );
+//
+//};
 
 public class Main {
     static void main() {
 
-System.out.println("hellow world");
 
-       OkHttpClient client= new OkHttpClient();
+//       OkHttpClient client= new OkHttpClient();
+//
+//        String url_string ="https://jsonplaceholder.typicode.com/todos/1";
+//
+//        Request request= new Request.Builder().url(url_string).build();
+//        try(Response response= client.newCall(request).execute() ){
+//            if(!response.isSuccessful()){
+//                System.out.println("something went wrong ");
+//            }
+//
+//            System.out.println(response.body().string());
+//
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+;
 
-        String url_string ="https://jsonplaceholder.typicode.com/todos/1";
 
-        Request request= new Request.Builder().url(url_string).build();
-        try(Response response= client.newCall(request).execute() ){
-            if(!response.isSuccessful()){
-                System.out.println("something went wrong ");
-            }
+        Retrofit retrofit =
+                new Retrofit.Builder()
+                        .baseUrl(
+                                "https://jsonplaceholder.typicode.com/")
+                        .addConverterFactory(
+                                GsonConverterFactory.create())
+                        .build();
 
-            System.out.println(response.body().string());
+        TodoService todoApi= retrofit.create(TodoService.class);
+        Call<Todo> call= todoApi.getTodoByID(5);
+        try {
+            Response<Todo> response = call.execute();
+            System.out.println(response.body());
 
-        } catch (Exception e) {
+
+
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
-;
+
     }
 }
