4224f01717bd956ae0fa0e4597809f31739bd487
Vivek Nishad <vivekmmmut2027@gmail.com>
2026-06-16 21:35:32 +0530

    add: configs for including dependencies classe ,implemented duplicate strategy

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,31 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import okhttp3.OkHttp;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+System.out.println("hellow world");
+
+       OkHttpClient client= new OkHttpClient();
+
+        String url_string ="https://jsonplaceholder.typicode.com/todos/1";
+
+        Request request= new Request.Builder().url(url_string).build();
+        try(Response response= client.newCall(request).execute() ){
+            if(!response.isSuccessful()){
+                System.out.println("something went wrong ");
+            }
+
+            System.out.println(response.body().string());
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
+;
     }
 }
